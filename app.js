f4ff7bd6af42bdef133ded22dd8aea75046a52d1
AK <mywork304@gmail.com>
2020-07-30 05:58:24 +0200

    make it better

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var app=express();
 mongoose.connect('mongodb+srv://AK:a01129292532@cluster0.uf0py.mongodb.net/booking?retryWrites=true&w=majority',{ useNewUrlParser: true,useUnifiedTopology: true },()=>{
     console.log('db connected');
 
-    const server=app.listen(process.env.Port||3000);
+    const server=app.listen(process.env.PORT||3000);
     require('./socket.io.settings').init(server)
    
      
