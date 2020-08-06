37a31bff397fd245310b107af6f8b80290fde593
AK <mywork304@gmail.com>
2020-08-06 20:35:42 +0200

    v2 deploy

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ mongoose.connect(remoteDB,{ useNewUrlParser: true,useUnifiedTopology: true },()=
      
 
 })
+app.use(express.static('images'))
 app.use('/images',express.static(path.join(__dirname,'images')));
 
 app=configMiddleware(app);
