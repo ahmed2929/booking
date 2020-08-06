3f4a0cac27b980e3eed93ff7a5cfc9376ccf0ab0
AK <mywork304@gmail.com>
2020-08-06 20:28:52 +0200

    v2 deploy

--- midllewares/config.js
+++ midllewares/config.js
@@ -17,7 +17,9 @@ module.exports=(app)=>{
    app.use(bodyParser.json());
    app.use(bodyParser.urlencoded({extended:true}));
    require('dotenv').config();
-   app.use('/images',express.static(path.join(__dirname,'/images')));
+   app.use('/images',express.static(path.join(__dirname,'..','images')));
+  //app.use('/static', express.static(path.join(__dirname, 'public')))
+
    app.get('/',(req,res)=>{
       res.send('welcome please read the docs')
    })
