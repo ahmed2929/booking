e14497a816e8e651d997916df6be0a7be0ae4b33
AK <mywork304@gmail.com>
2020-08-26 16:46:49 +0200

    ass

--- helpers/Auth/checkactivation.js
+++ helpers/Auth/checkactivation.js
@@ -1,13 +1,14 @@
 module.exports = async (req,res,next)=>{
-  
         if(req.user.emailVerfied==false){
             const error = new Error('sorry you need to activite your email first ');
             error.statusCode = 403 ;
             return next(error) ;
 
+        }else{
+            next();
         }
        
-        next();
+       
 
     
 };
