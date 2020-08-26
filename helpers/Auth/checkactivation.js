884e315f137044707c9c4e93c46fa4ed15b961d9
AK <mywork304@gmail.com>
2020-08-26 16:56:00 +0200

    ad

--- helpers/Auth/checkactivation.js
+++ helpers/Auth/checkactivation.js
@@ -1,21 +1,27 @@
+const jwt = require('jsonwebtoken');
+
+const User = require('../../models/TrederUsers');
+
 module.exports = async (req,res,next)=>{
     try{
-        if(req.user.emailVerfied==false){
-            const error = new Error('sorry you need to activite your email first ');
+      
+        if(user.user.emailVerfied==false){
+            const error = new Error('you need to activ your email first');
             error.statusCode = 403 ;
             return next(error) ;
 
-        }else{
-            next();
         }
-    }catch(err){
+            
+      
+       
+        next();
+
+    } catch(err){
         //console.debug(err)
         if(!err.statusCode){
             err.statusCode = 500;
         }
         return next(err);
     }
-       
-
     
 };
